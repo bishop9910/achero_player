f7422eb6186f10272609b993528ceaf3cb965729
bishop9910 <bishop9910@163.com>
2026-08-03 22:30:41 +0800

    test text

--- lib/main.dart
+++ lib/main.dart
@@ -109,6 +109,10 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Text(
+              "test text",
+              style: Theme.of(context).textTheme.titleSmall,
+            )
           ],
         ),
       ),
