98086a511ee8df5216c49811a3789d9208993885
bishop9910 <bishop9910@163.com>
2026-08-03 21:34:23 +0800

    change title and class name

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Achero());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Achero extends StatelessWidget {
+  const Achero({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Achero Player',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Achero Player Home Page'),
     );
   }
 }
